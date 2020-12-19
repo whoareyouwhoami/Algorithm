c6d231129b8c0853bf8d6632e0be5a16d463895a
whoareyouwhoami <young.woo@hotmail.com>
2020-12-19 22:12:49 +0900

    insertion sort

---
+++ src/com/algorithm/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.algorithm.sorting;
+
+public class InsertionSort {
+    private int[] insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
+            for(int j = i; j > 0; j--) {
+
+                if(array[j] < array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+    public static void main(String[] args) {
+        InsertionSort s = new InsertionSort();
+        int[] sorted = s.insertionSort(new int[] {1, 9, 6, 5, 10, 2, 19, 7});
+
+        System.out.println("Sorted: ");
+        for(int x: sorted) {
+            System.out.print(x + " ");
+        }
+    }
+}
